f80826ebbe208dd434a3c602eba7e20be470c520
Dmitrii Kudriavtsev <diak3@yandex.ru>
2021-02-03 19:34:13 +0400

    exploring azure function assests

---
+++ AzureFunctionsCleanArchitectureSample/AzureFunctionsCleanArchitectureSample.Api/CreateItem.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunctionsCleanArchitectureSample.Api
+{
+    public class CreateItem
+    {
+        [FunctionName("CreateItem1")]
+        public async Task<IActionResult> Run1(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "api/v1/func1")] HttpRequest req, 
+            ExecutionContext context,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("CreateItem2")]
+        public async Task<IActionResult> Run2(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "api/v1/func2")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
